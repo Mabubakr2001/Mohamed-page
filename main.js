e9ce17f09b09c5a7b6eec280bb86b3c498d7ce63
muhammed abubakr <mabobakr365@gmail.com>
2022-09-15 14:59:45 +0200

    Modified the functionality file

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var allUnits_span = document.querySelectorAll(".unit span");
-var myBirthday = new Date("Aug 6, 2023 23:59:59").getTime();
-var myCounter = setInterval(() => {
+var targetDate = new Date("Aug 6, 2023 23:59:59").getTime();
+var allUnits = document.querySelectorAll(".unit span");
+
+var birthdayCounter = setInterval(() => {
     var currentDate = new Date().getTime();
-    var reminDate = myBirthday - currentDate;
-    
+    var reminDate = targetDate - currentDate;
+
     var days = Math.floor(reminDate / (1000 * 60 * 60 * 24));
     document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
 
@@ -17,41 +18,55 @@ var myCounter = setInterval(() => {
     document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
 
     if (reminDate < 0){
-        clearInterval(myCounter)
-        allUnits_span.forEach(unit => unit.innerHTML = "00")
-        document.querySelector(".birthday-hint").innerHTML = "Oh, It's my birthday 🎊🎁🎈"
-        document.querySelector(".birthday-time").innerHTML = "Oh my god, I'm delighted right now, this moment is one of the happiest moments I've got in my life."
-    }
-}, 1000)
+        clearInterval(birthdayCounter);
+        allUnits.forEach(unit => unit.innerHTML = "00");
+        document.querySelector(".birthday-hint").innerHTML = "It's my birthday 🎊🎁🎈";
+        document.querySelector(".birthday-time").innerHTML = "Oh my god, I'm delighted right now, this moment is one of the happiest moments I've ever got in my life.";
+    };
+}, 1000);
 
-
-var skillsSection_section = document.querySelector(".skills");
-var allSkills_span = document.querySelectorAll(".progress span");
-var statsSection_section = document.querySelector(".stats");
-var allNumbers_span = document.querySelectorAll(".state .number");
+var upButton = document.querySelector(".go-to-up")
+var skillsSection = document.querySelector(".skills");
+var allSkills = document.querySelectorAll(".progress span");
+var statsSection = document.querySelector(".stats");
+var allNumbers = document.querySelectorAll(".state .number");
 var isfunctionStarted = false;
 
-function startCount(element){
+function startCount(element) {
     var targetNumber = element.dataset.target;
     var numberCounter = setInterval(() => {
         element.textContent++;
         if (element.textContent === targetNumber){
-            clearInterval(numberCounter)
+            clearInterval(numberCounter);
         }
-    }, 2000 / targetNumber)
-}
+    }, 1000 / targetNumber);
+};
+
 
 window.onscroll = function () {
-    if (window.scrollY >= statsSection_section.offsetTop - 2){
-        if (!isfunctionStarted){
-            allNumbers_span.forEach(number => startCount(number))
-        }
-        isfunctionStarted = true
+    if (this.scrollY >= 700){
+        upButton.classList.add("show-up--button")
+    }else{
+        upButton.classList.remove("show-up--button")
     }
 
-    if (window.scrollY >= skillsSection_section.offsetTop - 2){
-        allSkills_span.forEach(skill => {
-            skill.style.width = skill.dataset.width;
+    upButton.onclick = function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
         })
     }
-}
+
+    if (window.scrollY >= skillsSection.offsetTop - 2) {
+        allSkills.forEach(skill => {
+            skill.style.width = skill.dataset.width;
+        });
+    };
+
+    if (window.scrollY >= statsSection.offsetTop - 2){
+        if (!isfunctionStarted){
+            allNumbers.forEach(number => startCount(number));
+        }
+        isfunctionStarted = true;
+    };
+};
